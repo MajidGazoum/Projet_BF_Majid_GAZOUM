3e1994f46da544ed2c5bf2aac1d0cf9a18d73b71
MajidGazoum <146434549+MajidGazoum@users.noreply.github.com>
2024-05-16 02:37:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from models import (
+    get_portfolio_indicators,
+)
+from repository import get_data, get_weights, get_config, Calcul_Rendement, Calcul_Valeur_PF, Extract_rf
+from view import display_results
 
+
+def main():
+    config = get_config()
+    weights = get_weights(config)
+    data = get_data(config)
+    assets_returns = Calcul_Rendement(data)
+    PF_prices = Calcul_Valeur_PF(data, weights)
+    PF_returns = Calcul_Rendement(PF_prices)
+    rfr = Extract_rf()
+
+    # Calculation
+    indicators = get_portfolio_indicators(data, PF_returns, PF_prices, assets_returns, weights, rfr, config)
+
+    # Display results
+    display_results(indicators, config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ repository.py
@@ -0,0 +1,59 @@
+import os
+from typing import Dict
+
+import numpy as np
+import yfinance as yf
+import pandas as pd
+import fredapi
+
+from constants import CONFIG_FILE
+from helpers import get_toml_data
+
+
+def get_config() -> Dict:
+    path = os.path.join(os.getcwd(), CONFIG_FILE)
+    return get_toml_data(path)
+
+
+def get_weights(config: Dict) -> np.array:
+    return np.array(list(config["portfolio"].values()))
+
+
+def get_data(config: Dict):
+    tickers = list(config["portfolio"].keys())
+    data = yf.download(
+        tickers,
+        start=config["initialisation"]["begin_date"],
+        end=config["initialisation"]["end_date"],
+    )
+    return data[config["initialisation"]["field_to_keep"]]
+
+
+def Calcul_Rendement(dataframe: pd.DataFrame):
+    rendement = dataframe.pct_change()
+    rendement = rendement.dropna()
+    return rendement
+
+def Calcul_Valeur_PF(prix_actifs: pd.DataFrame, weights: np.array):
+    valeur = np.dot(prix_actifs.values, weights)
+    valeur = pd.DataFrame(valeur, columns=['Portefeuille'], index=prix_actifs.index)
+    return valeur
+
+def Extract_rf():
+    fred = fredapi.Fred(api_key='55cc7affbaba092c1f14f4fd882eaaf5')
+    us_treasury_10y = fred.get_series_latest_release('GS10') / 100
+    rfr = us_treasury_10y.iloc[-1]
+    return float(rfr)
+
+
+if __name__ == "__main__":
+    conf = get_config()
+    print(get_weights(conf))
+    results = get_data(conf)
+    print(results)
+    assets_returns = Calcul_Rendement(results)
+    PF_returns = Calcul_Valeur_PF(assets_returns, get_weights(conf))
+    print(assets_returns, PF_returns)
+    print(Extract_rf() * 100, '%')
+    print(conf)
+
